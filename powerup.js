4b1483fdcdd655c4c90ef2f105174e1b5121c775
Michael Delfino <michael.a.delfino@gmail.com>
2017-01-21 10:47:21 -0500

    Add powerup.js

---
+++ powerup.js
@@ -0,0 +1,20 @@
+function Powerup(info) {
+  this.x = info.x;
+  this.y = info.y;
+  this.size = 20;
+  this.alive = true;
+  this.ttl = 100;
+}
+
+Powerup.prototype.update = function() {
+  this.ttl--;
+  if (this.ttl <= 0) {
+    this.alive = false;
+  }
+}
+
+Powerup.prototype.draw = function() {
+  fill(Math.random() * 255, Math.random() * 255, Math.random() * 255);
+  noStroke();
+  ellipse(this.x, this.y, this.size);
+}
